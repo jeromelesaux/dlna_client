29c6a91100ac68f5106b8606a4ea26c549fbafcf
Jerome Le Saux <j.lesaux@axione.fr>
2017-11-02 16:50:40 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"github.com/huin/goupnp"
+	"github.com/huin/goupnp/dcps/av1"
+	"encoding/xml"
+)
+
+var servers = flag.Bool("servers", true, "list all the dlna servers availabled.")
+var list = flag.String("list", "", "get content from server uri")
+
+type DIDLLite struct {
+	XMLName xml.Name
+	DC      string   `xml:"xmlns:dc,attr"`
+	UPNP    string   `xml:"xmlns:upnp,attr"`
+	XSI     string   `xml:"xmlns:xsi,attr"`
+	XLOC    string   `xml:"xsi:schemaLocation,attr"`
+	Objects []Object `xml:"item"`
+}
+
+
+type Object struct {
+	ID         string `xml:"id,attr"`
+	Parent     string `xml:"parentID,attr"`
+	Restricted string `xml:"restricted,attr"`
+	Title string `xml:"title"`
+	Creator string `xml:"creator"`
+	Class string `xml:"class"`
+	Date string `xml:"date"`
+	Ress []Res `xml:"res"`
+}
+
+type Res struct {
+	Resolution string `xml:"resolution,attr"`
+	Size uint64 `xml:"size,attr"`
+	ProtocolInfo string `xml:"protocolInfo,attr"`
+	Duration string `xml:"duration,attr"`
+	Bitrate string `xml:"bitrate,attr"`
+	SampleFrequency uint64 `xml:"sampleFrequency"`
+	NrAudioChannels uint64 `xml:"nrAudioChannels"`
+	Value string `xml:",chardata"`
+}
+
+func main() {
+	flag.Parse()
+	if *servers == true {
+		devices, err := goupnp.DiscoverDevices("urn:schemas-upnp-org:device:MediaServer:1")
+		if err != nil {
+			log.Printf("error %v", err.Error())
+		} else {
+			for _, d := range devices {
+
+				//log.Printf("Device %v,%v",d.Root,d.Location)
+				clients, err := av1.NewContentDirectory1ClientsByURL(d.Location)
+				if err != nil {
+					log.Printf("Error while getting content directory client %v", err)
+				} else {
+					//for _,client := range clients {
+					client := clients[0]
+					result, returnNumber, totalMatches, update, err := client.Search("*", "", "*", 0, 0, "")
+					if err != nil {
+						log.Printf("Error while getting content directory client %v from location :%", err, d.Location)
+					} else {
+						log.Printf("result  %d, %d, %d", returnNumber, totalMatches, update)
+						r := &DIDLLite{}
+						err := xml.Unmarshal([]byte(result), r)
+						if err != nil {
+							log.Printf("Error while parsing result xml %v", err)
+						} else {
+							for _,item := range r.Objects {
+								 value := "nothing to display"
+								if len(item.Ress) > 0 {
+									value = item.Ress[0].Value
+								}
+								log.Printf("%s, %s, %s", item.Title,item.Class, value)
+							}
+						}
+					}
+
+					//}
+				}
+			}
+		}
+	} else {
+		flag.Usage()
+	}
+}
